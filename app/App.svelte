ce56e1c059a8b5d8462a1528ec72661d757336b4
Simon Moe <simmoe@gmail.com>
2020-03-26 08:48:06 +0100

    templating

--- app/App.svelte
+++ app/App.svelte
@@ -1,32 +1,5 @@
 <page actionBarHidden={true}>
-<tabs bind:selectedIndex={selectedTab}>
-    <tabStrip>
-        <tabStripItem>
-            <label text="Page 1" />
-        </tabStripItem>
-        <tabStripItem>
-            <label text="Page 2" />
-        </tabStripItem>
-        <tabStripItem>
-            <label text="Page 3" />
-        </tabStripItem>
-    </tabStrip>
-    <tabContentItem>
-        <cardView margin="32" elevation="40" shadowRadius="8" shadowOffsetWidth = '0'>
-            <Page1 />
-       </cardView>
-    </tabContentItem>
-    <tabContentItem>
-        <cardView margin="32" elevation="40" shadowRadius="8" shadowOffsetWidth = '0'>
-            <Page2 bind:selectedTab={selectedTab} />
-        </cardView>
-    </tabContentItem>
-    <tabContentItem>
-        <cardView margin="32" elevation="40" shadowRadius="8" shadowOffsetWidth = '0'>
-            <Page3 />
-        </cardView>
-    </tabContentItem>
-</tabs>
+    <Page1 />
 </page>
 
 <script>
@@ -37,9 +10,6 @@ registerNativeViewElement("cardView", () => require("@nstudio/nativescript-cardv
     import Page1 from './screens/Page1.svelte'
     import Page2 from './screens/Page2.svelte'
     import Page3 from './screens/Page3.svelte'
-
-    let pages = [Page1, Page2, Page3]
-    let selectedTab = 0
 
 </script>
 
--- app/screens/Page2.svelte
+++ app/screens/Page2.svelte
@@ -1,8 +1,4 @@
 <flexboxLayout class='flex-centered'>
     <label class='h1 text-center' text='page 2' />
-    <button on:tap={()=>selectedTab=0} text='page 1'/>
 </flexboxLayout>
 
-<script>
-    export let selectedTab 
-</script>
